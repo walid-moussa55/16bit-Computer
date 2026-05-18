6355d41d82d91f1356808e454ea7fbe4cc7f15cf
walid-moussa55 <walid.aitmoussa@edu.uiz.ac.com>
2026-05-18 10:40:16 +0100

    Update files

--- run.bat
+++ run.bat
@@ -1,24 +1,29 @@
 @echo off
 
 @REM echo Generator Instruction Table and Dictionary...
-@REM g++ ./Generator/generator.cpp -o ./Generator/generator.exe
-@REM .\Generator\generator.exe
+@REM g++ ./Generator/generator.cpp -o ./build/generator.exe
+@REM .\build\generator.exe
 @REM pause
 
 @REM echo Generator CU Unicode...
-@REM g++ ./Unicode_CU/unicode_cu.cpp -o ./Unicode_CU/unicode_cu.exe
-@REM .\Unicode_CU\unicode_cu.exe
+@REM g++ ./Unicode_CU/unicode_cu.cpp -o ./build/unicode_cu.exe
+@REM .\build\unicode_cu.exe
 @REM pause
 
 echo Generating Assembly code (Compilation)...
-@REM g++ .\Compiler\compiler.cpp -std=c++17 -o .\Compiler\compiler.exe
-.\Compiler\compiler.exe ./Code/program.tom ./Code/program.ass
+@REM g++ .\Compiler\compiler.cpp -std=c++17 -o .\build\compiler.exe
+.\build\compiler.exe ./Code/program.tom ./Code/program.ass
 pause
 
 echo Generating Program RAM Code...
-@REM g++ .\Assembler\assembler.cpp -o .\Assembler\assembler.exe
-.\Assembler\assembler.exe ./Code/program.ass
+@REM g++ .\Assembler\assembler.cpp -o .\build\assembler.exe
+.\build\assembler.exe ./Code/program.ass
 pause
+
+@REM echo Runing Simulator in Prgram Code...
+@REM g++ .\Simulator\simulator.cpp -o .\build\simulator.exe
+@REM .\build\simulator.exe ram_unicode --debug
+@REM pause
 
 @REM echo Clean up...
 @REM del temp.tom
